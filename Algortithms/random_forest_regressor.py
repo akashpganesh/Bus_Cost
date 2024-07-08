acac5df78bb6547da5489a6b1aacff1010aedb26
Akash P Ganesh <akashpganesh2002@gmail.com>
2024-07-08 14:26:58 +0530

    algorithm updates

--- Algortithms/random_forest_regressor.py
+++ Algortithms/random_forest_regressor.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import RandomForestRegressor
 
 def calculate_duration(start, end):
     start = pd.to_datetime(start, format='%H:%M')
@@ -29,7 +29,8 @@ def minimal_cost(data,cost):
 
     X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
 
-    model = GradientBoostingRegressor(n_estimators=100, learning_rate=0.1, max_depth=3, random_state=42)
+    model = RandomForestRegressor(n_estimators=100, max_depth=None, random_state=42)
+
     model.fit(X_train, y_train)
 
     y_pred = model.predict(X_test)
@@ -40,6 +41,6 @@ def minimal_cost(data,cost):
 
     df['cost_diff'] = np.abs(df['predicted_costs'] - cost)
     df_sorted = df.sort_values(by='cost_diff')
-    recommended_buses = df_sorted.head(25)
+    recommended_buses = df_sorted.head(20)
 
     return recommended_buses.to_dict(orient='records')
